508e63eee33ea4f806c7b936478605da967d3f86
ekomation <centurionsnr@gmail.com>
2023-03-08 15:51:40 +0100

    This is the initial commit

--- app/src/App.js
+++ app/src/App.js
@@ -29,7 +29,18 @@ function App() {
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const value = ethers.utils.parseEther(document.getElementById('wei').value);
+    //const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    //const value = ethers.BigNumber.from(document.getElementById('wei').value);
+   // const value = ethers.utils.formatEther(wei);
+
+
+
+
+   //const value = ethers.BigNumber.from(document.getElementById('wei').value).mul('1000000000000000000');
+ 
+
+
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
 
 
@@ -37,7 +48,7 @@ function App() {
       address: escrowContract.address,
       arbiter,
       beneficiary,
-      value: value.toString(),
+      value: (value / 1000000000000000000).toString() + ' ETH',
       handleApprove: async () => {
         escrowContract.on('Approved', () => {
           document.getElementById(escrowContract.address).className =
@@ -68,7 +79,7 @@ function App() {
         </label>
 
         <label>
-          Deposit Amount (in Wei)
+          Deposit Amount (in ETH)
           <input type="text" id="wei" />
         </label>
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,16 @@
-require('@nomicfoundation/hardhat-toolbox');
+//require('@nomicfoundation/hardhat-toolbox');
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
 
 module.exports = {
-  solidity: "0.8.17",
-  paths: {
-    artifacts: "./app/src/artifacts",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/WA5kzdkfRExcOuysOOPvOLSSTmnzBXtx',
+      accounts: ['72b7671eacc63b311b8504652dadd72b36909e45b9ff17a59c59f3951b0004d6']
+    },
   }
 };
+
+
